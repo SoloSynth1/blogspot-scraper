54cb98c00329b5b4e3dcbec349ae01df43244b0c
SoloSynth1 <orixay@hotmail.com>
2021-10-04 11:53:49 +0800

    reorganizing examples into examples/

--- marcoramilli.blogspot.com.py
+++
@@ -1,6 +0,0 @@
-from crawler.crawler import BlogspotCrawler
-import os
-
-if __name__ == "__main__":
-    bc = BlogspotCrawler(2940307687099594687, os.path.join('.','data', 'marcoramilli.blogspot.com','posts.json'))
-    bc.crawl()
